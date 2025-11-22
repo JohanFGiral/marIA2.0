b699d1a905ef8c830184c9588f7d830af1bc0bdd
JOHAN NICOLAS FORERO GIRAL <johanelprinci4@gmail.com>
2025-11-22 00:58:00 -0500

    changes api

--- Backend/modules/hugging_face.py
+++ Backend/modules/hugging_face.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 client = OpenAI(
     base_url="https://router.huggingface.co/v1",
-    api_key="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",
+    api_key=os.environ.get("HF_TOKEN_KEY"),
 )
 
 h_messages=[
--- Backend/routes/routes.py
+++ Backend/routes/routes.py
@@ -4,15 +4,30 @@ from modules import hugging_face, tts
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=[
+    "http://127.0.0.1:5500",
+    "https://marIA2.0.github.io",
+    "http://localhost:5500"  # por si usas localhost en vez de 127.0.0.1
+])
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 AUDIO_FOLDER = os.path.join(os.path.dirname(os.path.dirname(__file__)), "audios")
+# FRONTEND_DIR = carpeta Frontend al mismo nivel que Backend
+FRONTEND_DIR = os.path.join(BASE_DIR, '..','..' ,'Frontend')  
 
 @app.route('/')
 def inicio():
-    return 'Bienvenido a Flask'
+    html_path = os.path.join(FRONTEND_DIR, 'MarIA.html')
+    if not os.path.exists(html_path):
+        return f"Archivo no encontrado en: {html_path}", 404
+    return send_from_directory(FRONTEND_DIR, 'MarIA.html')
 
+@app.route('/<path:path>')
+def frontend_files(path):
+    file_path = os.path.join(FRONTEND_DIR, path)
+    if not os.path.exists(file_path):
+        return f"Archivo no encontrado: {file_path}", 404
+    return send_from_directory(FRONTEND_DIR, path)
 @app.route('/send', methods=['POST'])
 def message():
     user_message = request.json.get("mensaje")
@@ -23,11 +38,12 @@ def message():
     respuesta = hugging_face.send_messages(user_message)
     
     audio_filename = tts.text_to_speech(respuesta)
+    host_url = request.host_url
     print("Audio generado:", audio_filename)
 
     return jsonify({
         "respuesta": respuesta,
-        "audio_url": f"http://127.0.0.1:5000/audio/{audio_filename}"
+        "audio_url": f"{host_url}audio/{audio_filename}"
     })
 
 @app.route('/audio/<path:filename>')
