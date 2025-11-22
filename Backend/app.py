54bccbaef6e8ba1dd73b290f0d96266fb32b8f52
JOHAN NICOLAS FORERO GIRAL <johanelprinci4@gmail.com>
2025-11-22 00:34:09 -0500

    last commit

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,11 @@
-from routes.routes import app
 import sys
 import os
+
+# Asegurarse de que el directorio actual esté en el path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from routes.routes import app
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
